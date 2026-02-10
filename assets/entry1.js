28ec345de5cc04ad8d7de24fd24227612c3cd7dd
MinseoKim <kimm222@newschool.edu>
2026-02-10 09:17:11 -0500

    link

--- assets/entry1.js
+++ assets/entry1.js
@@ -1,4 +1,4 @@
-const THRESHOLD = 70; // 이만큼 스크롤하면 2번 레이아웃로 전환
+const THRESHOLD = 100; // 이만큼 스크롤하면 2번 레이아웃로 전환
 
 function onScroll() {
   document.body.classList.toggle("is-scrolled", window.scrollY > THRESHOLD);
