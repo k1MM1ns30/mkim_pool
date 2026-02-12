d02e20e78723dcf0f8d95a15ee40a3fb95747c1b
MinseoKim <kimm222@newschool.edu>
2026-02-12 11:53:47 -0500

    mouse

--- assets/entry2.js
+++ assets/entry2.js
@@ -6,3 +6,15 @@ function onScroll() {
 
 window.addEventListener("scroll", onScroll);
 onScroll(); // 처음 로드 시에도 체크
+
+document.addEventListener("pointerdown", () => {
+  document.body.classList.add("is-clicking");
+});
+
+document.addEventListener("pointerup", () => {
+  document.body.classList.remove("is-clicking");
+});
+
+document.addEventListener("pointercancel", () => {
+  document.body.classList.remove("is-clicking");
+});
